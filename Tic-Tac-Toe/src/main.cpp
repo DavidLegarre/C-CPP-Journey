30c25bc1e8dab9b50c964f0a081852999f2957ba
david <davidls920@gmail.com>
2023-02-24 14:50:03 +0100

    Created First Makefile

--- Tic-Tac-Toe/src/main.cpp
+++ Tic-Tac-Toe/src/main.cpp
@@ -1,6 +1,16 @@
+#include "rendering.hpp"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!\n";
+    // Define Variables
+    const int width = 3;
+    const int height = 3;
+
+    // Store the state of the board
+    //int *gamestate = new int[width*height];
+    int gamestate[height][width];
+
+
+
     return 0;
 }
