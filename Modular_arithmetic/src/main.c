badc96e9258b507c29161a44e9596c3453cb7d99
davidlegarre <davidls920@gmail.com>
2023-03-15 20:34:37 +0100

    Modular exponenciation made in a loop

--- Modular_arithmetic/src/main.c
+++ Modular_arithmetic/src/main.c
@@ -4,27 +4,41 @@
 
 int main()
 {
-  int base;
-  int exp;
-  int mod;
+  char cont;
+  int running = 1;
+  while (running)
+  {
+    int base;
+    int exp;
+    int mod;
 
-  printf("Base: ");
-  scanf("%d", &base);
+    printf("Base: ");
+    scanf("%d", &base);
 
-  printf("Exponent: ");
-  scanf("%d", &exp);
+    printf("Exponent: ");
+    scanf("%d", &exp);
 
-  printf("Modulo: ");
-  scanf("%d", &mod);
+    printf("Modulo: ");
+    scanf("%d", &mod);
 
-  int result;
+    int result;
 
-  printf("base %d, exponent %d, modulo %d\n", base, exp, mod);
+    printf("base %d, exponent %d, modulo %d\n", base, exp, mod);
 
-  // result = binary_exponentiation(base, exp, mod);
-  result = binary_exp_steps(base, exp, mod);
+    // result = binary_exponentiation(base, exp, mod);
+    result = binary_exp_steps(base, exp, mod);
 
-  printf("%d^%d = %d mod %d\n", base, exp, result, mod);
-
+    printf("%d^%d = %d mod %d\n", base, exp, result, mod);
+    printf("Run again? Y/N\n");
+    scanf("%s", &cont);
+    if (cont == 'y' || cont == 'Y')
+    {
+      running = 1;
+    }
+    else
+    {
+      running = 0;
+    }
+  }
   return 0;
 }
