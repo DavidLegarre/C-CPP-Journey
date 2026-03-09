2cdb50b4f70993e5689c3a449a2dd8874c7617c0
davidlegarre <davidls920@gmail.comm>
2026-03-09 09:24:08 +0100

    Refactor Snake project structure and add initial configuration files

---
+++ snake/src/domain/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIDTH 20
+#define HEIGHT 20
+#define INITIAL_LENGTH 3
+
+#endif
---
+++ snake/src/domain/snake.h
@@ -0,0 +1,10 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <stdbool.h>
+
+typedef struct {
+    int x, y;
+} Vec2;
+
+#endif
